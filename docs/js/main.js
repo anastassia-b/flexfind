f78f2eedd22037f80cdd42de630c5cfc0f799007
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-09-24 20:11:51 -0700

    Add react and dependencies

---
+++ docs/js/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const json = require('../../data/data.json');
+
+console.log(json);
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,20 @@ module.exports = {
   entry: './docs/js/main.js',
   output: {
     filename: './docs/js/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
   }
 };
